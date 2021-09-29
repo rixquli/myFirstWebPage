c05cc3f9a3f8c515ea081fad950106c4bd829181
rixquli <81580332+rixquli@users.noreply.github.com>
2021-09-29 16:12:36 +0200

    Add files via upload

--- Js/javapage1.js
+++ Js/javapage1.js
@@ -1,4 +1,25 @@
 function menuOpen(){
- var element=document.getElementById("menuid")
- element.innerHTML="";
+ var element= document.getElementById("menuId");
+ element.innerHTML=
+`<a href="Index/pythagore.html">Pythagore </a></br>
+ Thales </br>
+ <a href="https://youtu.be/dQw4w9WgXcQ>Autre</a>`;
+  element.style.position= "absolute";
+  element.style.top= "7px";
+  element.style.right= "2px"
+  element.style.diplay= "flex";
+  element.style.flexDirection= "column";
+  element.style.border= "solid";
+  element.style.borderSize= "1px";
+  element.style.backgroundColor= "#00c4bf";
+  let elementB= document.getElementById("menuIdButtonOpen");
+  elementB.setAttribute("onclick", "menuClose()");
+}
+
+function menuClose(){
+  var element= document.getElementById("menuId");
+  element.innerHTML= "";
+  let elementB= document.getElementById("menuIdButtonOpen");
+  elementB.setAttribute("onclick", "menuOpen()");
+  element.style.border= "none";
 }
